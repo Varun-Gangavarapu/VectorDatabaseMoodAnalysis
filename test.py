a0b6077bf72601a0d3282d707cffdca5925b7dae
Satvik Srinivas <satviksrinivas@nbp-27-74.nbp.ruw.rutgers.edu>
2024-03-23 21:03:40 -0400

    hella vector_db stuff

---
+++ test.py
@@ -0,0 +1,12 @@
+
+from vec_db import Vec_DB
+from preprocess import compute_user_vector_sentiments
+import numpy as np
+
+if __name__ == '__main__':
+    vec_db = Vec_DB('7f339788-9a19-440f-a013-bd2cc0cb73f1')
+    print(
+        vec_db.get_knn(
+            compute_user_vector_sentiments('dataRecentlyPlayed.json'),
+            k=5
+        ))
